a415cca8360177ac9a99493eea9684be4681025a
xichuchu <xichuchu@saicmotor.com>
2020-10-10 16:14:51 +0800

    准备日志,demo

--- demoC/demoA.c
+++ demoC/demoA.c
@@ -1,6 +1,7 @@
 
 #include"demoA.h"
 #include <stdio.h>
+int demoAValue;
 void printDemoA(){
 
 printf("the demoA is:%d,in demoA",demoAValue);
--- demoC/demoB.c
+++ demoC/demoB.c
@@ -2,6 +2,7 @@
 #include "demoA.h"
 #include "demoB.h"
 #include <stdio.h>
+int demoAValue;
 void printDemoAForB(){
 
 printf("the demoA is:%d,in demoB",demoAValue);
