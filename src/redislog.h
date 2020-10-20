ec31fdc209d7327daf727ea0115d60aadcc4179c
cyzi <lovehui131125@gmail.com>
2020-10-20 21:26:57 +0800

    c-style string 测试

--- src/redislog.h
+++ src/redislog.h
@@ -21,7 +21,7 @@
 
 #define CYZI_REDIS_SERVER_ABSTRACT_PATH "/home/yunchu/redis-cyzi/src/redis-server"
 
-#define ADDR2LINE_COMMAND_TEMPLATE "addr2line -a %s -e %s -f -C -p;"
+#define ADDR2LINE_COMMAND_TEMPLATE "addr2line %s -e %s -f -C -p;"
 
 
 void cyziServerLog(int loglevel,char * message,...);
