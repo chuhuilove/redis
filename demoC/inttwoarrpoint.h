79a69d56013bacb8029653f5ed3bb4ab7748ca3f
xichuchu <xichuchu@saicmotor.com>
2020-10-20 13:06:30 +0800

    c语言字符串数组-测试

--- demoC/inttwoarrpoint.h
+++ demoC/inttwoarrpoint.h
@@ -4,7 +4,7 @@
 #include "genarr.h"
 
 void printfArr(int (*ptr)[INR_ARR_COLS_LENGTH]);
-void plusvalue(int *ptr[INR_ARR_COLS_LENGTH],int plusValue);
+void plusvalue(int (*ptr)[INR_ARR_COLS_LENGTH],int plusValue);
 
 
 
